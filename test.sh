0ce31e66f921691a7934ed095b08fd6068698410
James Stevenson <james.m.stevenson@gmail.com>
2010-12-17 10:38:11 -0500

    Adds timing of solutions to test script.

--- test.sh
+++ test.sh
@@ -1,11 +1,19 @@
 #!/bin/sh 
 
 # First argument is the number, prints the result in haskell and then in python
-
+echo ""
 ghc --make $1
+echo "----------------------------"
 echo "Haskell solution:"
+start="$(date +%s)"
 ./$1
+stop="$(date +%s)"
+echo ""
+echo "Took" $(expr $stop - $start) "seconds in Haskell"
+echo "----------------------------"
 echo "Python solution:"
+start="$(date +%s)"
 python "$1.py"
-
-# FIXME: The ability to time these would be nice...
+stop="$(date +%s)"
+echo ""
+echo "Took" $(expr $stop - $start) "seconds in Python"
